8483e980803a1bc8f50d48d8fa0228d3ba4e3de7
jiyeonyun <jiyeonyun43@gmail.com>
2022-03-02 21:57:48 +0900

    login&logout function

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,18 @@
 import Login from './components/login/login';
 import styles from './app.module.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Maker from './components/maker/maker';
+
 function App({authService}) {
   return (
     <div className={styles.app}>
-    <Login authService={authService}/>
+    <BrowserRouter>
+        <Routes>
+          <Route exact path='/' element={<Login authService={authService}/>} />
+          <Route path='/maker' element={<Maker authService={authService}/>}/>
+        </Routes>
+    </BrowserRouter>
     </div>
   );
 }
