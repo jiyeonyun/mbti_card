ead5f61231dad3f682299f167f2941aadc06a42b
jiyeonyun <jiyeonyun43@gmail.com>
2022-03-04 22:49:43 +0900

    card components

---
+++ src/components/card/card.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import styles from './card.module.css';
+
+const DEFAULT_IMAGE = '/images/default_logo.png';
+
+const Card = ({card}) =>  {
+    const {name, company, title, email, message, theme, fileName ,fileURL} = card;
+    const url = fileURL || DEFAULT_IMAGE;
+    
+    return(
+        <li className={styles.card}>
+            <img className={styles.avatar} src={url} alt="profile photo" />
+            <div className={styles.info}>
+                <h1 className={styles.name}>{name}</h1>
+                <p className={styles.company}>{company}</p>
+                <p className={styles.title}>{title}</p>
+                <p className={styles.email}>{email}</p>
+                <p className={styles.message}>{message}</p>
+            </div>
+        </li>
+    );
+};
+
+export default Card;
