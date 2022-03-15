132ab15b00cefbd9875298902d5384f696e48a5c
jiyeonyun <jiyeonyun43@gmail.com>
2022-03-15 15:45:58 +0900

    card repository

--- src/app.jsx
+++ src/app.jsx
@@ -4,13 +4,13 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Maker from './components/maker/maker';
 
-function App({FileInput,authService}) {
+function App({FileInput,authService,cardRepository}) {
   return (
     <div className={styles.app}>
     <BrowserRouter>
         <Routes>
           <Route exact path='/' element={<Login authService={authService}/>} />
-          <Route path='/maker' element={<Maker authService={authService} FileInput={FileInput}/>}/>
+          <Route path='/maker' element={<Maker authService={authService} FileInput={FileInput} cardRepository={cardRepository}/>}/>
         </Routes>
     </BrowserRouter>
     </div>
